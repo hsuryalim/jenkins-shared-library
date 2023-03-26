dd321eba637182721683abc7fbe627d57be7bfa1
ts-hanjaya.suryalim <ts-hanjaya.suryalim@rakuten.com>
2023-03-26 22:12:39 +0900

    triggering jenkins job

---
+++ src/com/acceleratedskillup/Car.groovy
@@ -0,0 +1,22 @@
+package com.acceleratedskillup;
+
+class Car {
+    int numberOfDoors
+    String brand
+    String model
+    def script
+
+    Car(script) {
+        this.script = script
+    }
+
+    void printOutCar() {
+        script.echo "number of doors: ${numberOfDoors}"
+        script.echo "model: ${model}"
+        script.echo "brand: ${brand}"
+    }
+
+    void printOutCar2() {
+        println "number of doors is ${numberOfDoors}"
+    }
+}
