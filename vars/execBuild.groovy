f9bfaf78b908e64be51a84c6da6fc1dd546ce37d
ts-hanjaya.suryalim <ts-hanjaya.suryalim@rakuten.com>
2023-03-28 00:17:45 +0900

    triggering jenkins job

---
+++ vars/execBuild.groovy
@@ -0,0 +1,48 @@
+def call(String env) {
+    if (env == 'Build') {
+        pipeline {
+            agent {
+                docker {
+                    image 'golang:alpine'
+                }
+            }
+
+            stages {
+                stage('Build') {
+                    steps {
+                        sh "echo Build"
+                    }
+                }
+            }
+        }
+    }
+
+    else if (env == 'Deploy') {
+        pipeline {
+            agent {
+                docker {
+                    image 'alpine'
+                }
+            }
+
+            stages{
+                stage('Deploy') {
+                    steps {
+                        sh "echo Deploy"
+                    }
+                }
+            }
+        }
+    }
+
+    else {
+        pipeline {
+            agent any
+            stages {
+                stage('Else') {
+                    sh "echo Else..."
+                }
+            }
+        }
+    }
+}
